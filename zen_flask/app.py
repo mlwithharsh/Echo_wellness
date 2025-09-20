edca1dbf55d871c08e08722aa28208adb484ecf7
Shubhajeet <magicaldanger6677@gmail.com>
2025-09-20 16:11:29 +0530

    app log

--- zen_flask/app.py
+++ zen_flask/app.py
@@ -419,16 +419,6 @@ else:
     # Production mode - configure logging
     if not app.debug:
         import logging
-        from logging.handlers import RotatingFileHandler
         
-        if not os.path.exists('logs'):
-            os.mkdir('logs')
-        
-        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
-        ))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
         app.logger.setLevel(logging.INFO)
         app.logger.info('EchoV1 Flask app startup')
